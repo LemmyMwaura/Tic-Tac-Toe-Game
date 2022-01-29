f5a192ea411c603f391e5409b5e06ef265338903
Lemmy Mwaura <lemminton@gmail.com>
2022-01-29 15:03:04 +0300

    improved functionality, stored winning combinations

--- script.js
+++ script.js
@@ -2,7 +2,17 @@
 const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board');
 const X_CLASS = 'x';
-const CIRCLE_CLASS = "circle";
+const CIRCLE_CLASS = 'circle';
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 let circleTurn;
 startGame();
 function startGame() {
@@ -17,8 +27,12 @@ function handleClickEvent(e) {
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
     placeMark(cell, currentClass);
     //Check for win
+    if (checkWin) {
+        console.log('winner');
+    }
     //Check for Draw
     swapTurns();
+    setBoardHoverClass();
 }
 function placeMark(cell, currentClass) {
     cell.classList.add(currentClass);
@@ -27,12 +41,19 @@ function swapTurns() {
     circleTurn = !circleTurn;
 }
 function setBoardHoverClass() {
-    board.classList.remove(CIRCLE_CLASS);
     board.classList.remove(X_CLASS);
+    board.classList.remove(CIRCLE_CLASS);
     if (circleTurn) {
         board.classList.add(CIRCLE_CLASS);
     }
     else {
         board.classList.add(X_CLASS);
     }
+}
+function checkWin(currentClass) {
+    return WINNING_COMBINATIONS.some((combinations) => {
+        return combinations.every(index => {
+            return cellElements[index].classList.contains(currentClass);
+        });
+    });
 }
--- script.ts
+++ script.ts
@@ -1,7 +1,17 @@
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
 const X_CLASS ='x'
-const CIRCLE_CLASS = "circle"
+const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
 let circleTurn:any
 
 startGame()
@@ -19,8 +29,12 @@ function handleClickEvent(e:any, ): void {
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
     placeMark(cell, currentClass)
    //Check for win
+   if (checkWin) {
+    console.log('winner')
+   }
    //Check for Draw
     swapTurns()
+    setBoardHoverClass()
 }
 
 function placeMark(cell:any, currentClass:any){
@@ -32,12 +46,19 @@ function swapTurns(){
 }
 
 function setBoardHoverClass(){
-    board!.classList.remove(CIRCLE_CLASS)
     board!.classList.remove(X_CLASS)
-
+    board!.classList.remove(CIRCLE_CLASS)
     if(circleTurn){
         board!.classList.add(CIRCLE_CLASS)
     } else{
         board!.classList.add(X_CLASS)
     }
+}
+
+function checkWin(currentClass:any){
+    return WINNING_COMBINATIONS.some( (combinations) => {
+        return combinations.every(index => {
+            return cellElements[index].classList.contains(currentClass)
+        })
+    })
 }
